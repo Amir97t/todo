3d5e248d686f6f8e3a392d1b74bc5bd10388326c
Amir <amir.taghizade76@gmail.com>
2026-07-23 12:45:58 +0330

    refactor:  add new style & logic

--- src/components/task/FIlterBar.jsx
+++ src/components/task/FIlterBar.jsx
@@ -1,11 +1,22 @@
-import Button from "../ui/Button";
-
-export default function FilterBar({ onChange }) {
+export default function FilterBar({ filter, onChange }) {
+  const btn = (value, label) => (
+    <button
+      onClick={() => onChange(value)}
+      className={`rounded-lg px-4 py-2 transition
+            ${
+              filter === value
+                ? "bg-red-600 text-white"
+                : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
+            }`}
+    >
+      {label}
+    </button>
+  );
   return (
-    <div className="flex gap-3">
-      <Button onClick={() => onChange("all")}>All</Button>
-      <Button onClick={() => onChange("active")}>Active</Button>
-      <Button onClick={() => onChange("complete")}>Completed</Button>
+    <div className="mb-6 flex gap-3">
+      {btn("all", "All")}
+      {btn("newest", "Newest")}
+      {btn("oldest", "Oldest")}
     </div>
   );
 }
